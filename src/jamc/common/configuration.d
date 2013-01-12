234f7bcd90bf448d3385ffebdd7941efcf47615c
Zdeněk Biberle <xbiber00@stud.fit.vutbr.cz>
2013-01-12 02:09:31 +0100

    Čtení konfigurace už jede. Musel jsem to mírně obejít, možná jsme totiž narazili na bug kompilátoru, ještě to prubnu.

--- src/jamc/common/configuration.d
+++ src/jamc/common/configuration.d
@@ -11,35 +11,34 @@ import jamc.common.logger;
 import jamc.common.game;
 
 struct ServerConf {
-  string servername;
-  int port;
+    string servername;
+    int port;
 }
 
 void loadConfiguration( T )( IGame game, out T data, string filename ){
-  try{
-    string file = cast(string) std.file.read( filename );
-    std.xml.check(file);
-    DocumentParser xml = new DocumentParser(file);
-    
-    foreach( member; __traits(allMembers, T) ){
-      xml.onEndTag[ member ] = (in Element e) { // BUG!
+    try{
+        string file = cast(string) read( filename );
+        check(file);
+        auto xml = new DocumentParser( file );
         
-        writeln("member=" ~ member); // vypise pokazde toho sameho clena!
-        // misto toho lze pouzit e.tag.name, ale na to nelze pouzit typeof()
+        void onEnd( string target )( in Element e )
+        {
+            __traits( getMember, data, target ) = to!( typeof( __traits( getMember, data, target ) ) )( e.text() );
+        }
         
-        __traits(getMember, data, member) = to!( typeof( __traits(getMember,data,member) ) )( e.text() );
-        
-      };
+        foreach( member; __traits(allMembers, T) )
+        {
+            xml.onEndTag[ member ] = &( onEnd!( member ) );
+        }
+
+        xml.parse();
     }
-    
-    xml.parse();
-  }
-  catch(FileException e){
-    game.logger.error( "can't open configuration file: " ~ filename );
-  }
-  catch(CheckException e){
-    game.logger.error( "bad validation result of configuration file: " ~ filename );
-  }
-  game.logger.notice("configuration loaded");
+    catch(FileException e){
+        game.logger.error( "can't open configuration file: " ~ filename );
+    }
+    catch(CheckException e){
+        game.logger.error( "bad validation result of configuration file: " ~ filename );
+    }
+    game.logger.notice("configuration loaded");
 }
 
